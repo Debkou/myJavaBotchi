d817ac98063537f67b1c0d7532589169b9e68106
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-18 23:16:36 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
-
-
 /// <reference types="@workadventure/iframe-api-typings" />
 
 console.log('Script started successfully');
+
+// Globale Deklaration der noteWebsite-Variable
+let noteWebsite: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
 
-    let noteWebsite: any;
-
     WA.room.onEnterLayer("visibleNote").subscribe(async () => {
         console.log("Entering visibleNote layer");
+
+        // Überprüfen, ob noteWebsite bereits geöffnet ist und es schließen
+        if (noteWebsite) {
+            noteWebsite.close();
+            noteWebsite = undefined;
+        }
 
         noteWebsite = await WA.ui.website.open({
             url: "./note.html",
@@ -32,7 +37,10 @@ WA.onInit().then(() => {
     });
 
     WA.room.onLeaveLayer("visibleNote").subscribe(() => {
-        noteWebsite.close();
+        if (noteWebsite) {
+            noteWebsite.close();
+            noteWebsite = undefined;  // Setze die Variable auf undefined, um den Zustand zu aktualisieren
+        }
     });
 
 }).catch(e => console.error(e));
