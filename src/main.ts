a866fa3ac67e687cc8e7ff929bcd2317f9d1b725
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-18 22:58:51 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
+let noteWebsite: any = undefined;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
@@ -24,7 +25,6 @@ WA.onInit().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
-       let noteWebsite: any;
 
     WA.room.onEnterLayer("visibleNote").subscribe(async () => {
         console.log("Entering visibleNote layer");
@@ -45,12 +45,12 @@ WA.onInit().then(() => {
             allowApi: true,
         });
 
-
+       });
 
     WA.room.onLeaveLayer("visibleNote").subscribe(() => {
         noteWebsite.close();
     });
-    });
+ 
 }).catch(e => console.error(e));
 
 function closePopup(){
