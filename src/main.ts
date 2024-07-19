ef6c5c72424754ea3aa7123396d6dae83c26db81
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-19 15:59:46 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,41 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 console.log('Script started successfully');
- let noteWebsite: any;
+
+// Deklarieren Sie die Variable noteWebsite und die Konfigurationsparameter außerhalb der WA.onInit()-Funktion
+let noteWebsite: any;
+
+const websiteConfig = {
+    url: "src/note.html",
+    position: {
+        vertical: "top",
+        horizontal: "middle",
+    },
+    size: {
+        height: "30vh",
+        width: "50vw",
+    },
+    margin: {
+        top: "10vh",
+    },
+    allowApi: true,
+};
+
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
 
-
-
     WA.room.onEnterLayer("visibleNote").subscribe(async () => {
         console.log("Entering visibleNote layer");
 
-        noteWebsite = await WA.ui.website.open({
-            url: "src/note.html",
-            position: {
-                vertical: "top",
-                horizontal: "middle",
-            },
-            size: {
-                height: "30vh",
-                width: "50vw",
-            },
-            margin: {
-                top: "10vh",
-            },
-            allowApi: true,
-        });
+        noteWebsite = await WA.ui.website.open(websiteConfig);
     });
 
     WA.room.onLeaveLayer("visibleNote").subscribe(() => {
-      noteWebsite.close();
+        if (noteWebsite && typeof noteWebsite.close === 'function') {
+            noteWebsite.close();
+            noteWebsite = null; // Setzen Sie die Variable auf null, um anzuzeigen, dass sie geschlossen wurde
+        }
     });
 
 }).catch(e => console.error(e));
