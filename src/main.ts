99521dd2deabcdb3ad0bf3c353c647c1ddf68071
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-19 15:41:26 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -26,11 +26,24 @@ WA.onInit().then(() => {
             },
             allowApi: true,
         });
-
     });
 
     WA.room.onLeaveLayer("visibleNote").subscribe(() => {
-        noteWebsite.close();
+                noteWebsite = await WA.ui.website.open({
+            url: "src/note.html",
+            position: {
+                vertical: "top",
+                horizontal: "middle",
+            },
+            size: {
+                height: "80vh",
+                width: "50vw",
+            },
+            margin: {
+                top: "10vh",
+            },
+            allowApi: true,
+        });
     });
 
 }).catch(e => console.error(e));
