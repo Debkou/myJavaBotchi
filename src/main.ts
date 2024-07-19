3c27a8611f29f4bea3648ec3ec523da64d84074b
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-19 17:01:14 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,31 @@
 
 console.log('Script started successfully');
 
-// Deklarieren Sie die Variable noteWebsite außerhalb der WA.onInit()-Funktion
-let noteWebsite: any;
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+let currentPopup: any = undefined;
+let noteWebsite: any = undefined;
+
+// Funktion zum Schließen des Popups
+function closePopup(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
+    console.log('Player tags: ', WA.player.tags);
+
+    WA.room.area.onEnter('clock').subscribe(() => {
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    });
+
+    WA.room.area.onLeave('clock').subscribe(closePopup);
 
     WA.room.onEnterLayer("visibleNote").subscribe(async () => {
         console.log("Entering visibleNote layer");
@@ -36,6 +55,11 @@ WA.onInit().then(() => {
             noteWebsite = null; // Setzen Sie die Variable auf null, um anzuzeigen, dass sie geschlossen wurde
         }
     });
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
 
