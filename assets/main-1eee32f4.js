09415d16fcb8868d15e56f84ab58d7c16d3f5da1
Debkou <105669754+Debkou@users.noreply.github.com>
2024-07-19 14:49:22 +0000

    Deploying to gh-pages from master @ 8cebc523b32c17cc03611e7aca9b5b539b6c17f3 🚀

--- assets/main-1eee32f4.js
+++
@@ -1,2 +0,0 @@
-console.log("Script started successfully");let e;WA.onInit().then(()=>{console.log("Scripting API ready"),WA.room.onEnterLayer("visibleNote").subscribe(async()=>{console.log("Entering visibleNote layer"),e=await WA.ui.website.open({url:"./note.html",position:{vertical:"top",horizontal:"middle"},size:{height:"30vh",width:"50vw"},margin:{top:"10vh"},allowApi:!0})}),WA.room.onLeaveLayer("visibleNote").subscribe(()=>{e&&typeof e.close=="function"&&(e.close(),e=null)})}).catch(o=>console.error(o));
-//# sourceMappingURL=main-1eee32f4.js.map
