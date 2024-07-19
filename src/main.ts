276d6db2ca9567d0a5da06d726e65230ef52df09
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-19 16:08:06 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ WA.onInit().then(() => {
         console.log("Entering visibleNote layer");
 
         noteWebsite = await WA.ui.website.open({
-            url: "src/note.html",
+            url: "https://github.com/Debkou/myJavaBotchi/blob/master/src/note.html",
             position: {
                 vertical: "top",
                 horizontal: "middle",
