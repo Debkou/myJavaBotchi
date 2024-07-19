f7aed5a47cd415a07bf81a4b09edf50e4888a767
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-19 16:33:40 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,32 @@
 /// <reference types="@workadventure/iframe-api-typings" />
  let noteWebsite: any;
 
-    WA.room.onEnterLayer("visibleNote").subscribe(async () => {
-        console.log("Entering visibleNote layer");
-
-        noteWebsite = await WA.ui.website.open({
-            url: "src/note.html",
-            position: {
-                vertical: "top",
-                horizontal: "middle",
-            },
-            size: {
-                height: "30vh",
-                width: "50vw",
-            },
-            margin: {
+const myWebsite = await WA.ui.website.open({
+    url: "https://wikipedia.org",
+    position: {
+        vertical: "top",
+        horizontal: "middle",
+    },
+    size: {
+        height: "50vh",
+        width: "50vw",
+    },
+    margin: {
                 top: "10vh",
             },
             allowApi: true,
-        });
+});
+
+
+    WA.room.onEnterLayer("visibleNote").subscribe(async () => {
+        console.log("Entering visibleNote layer");
+
+       await WA.ui.website.open(myWebsite)
 
     });
 
     WA.room.onLeaveLayer("visibleNote").subscribe(() => {
-        noteWebsite.close();
+        mywebsite.close();
     });
 
 
