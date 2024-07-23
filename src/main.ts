ab2f4ad40d62d767aa3b99f997435d961bc741a2
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-23 16:00:59 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ WA.onInit().then(() => {
         console.log("Entering visibleNote layer");
 
         noteWebsite = await WA.ui.website.open({
-            url: "https://github.com/Debkou/myJavaBotchi/blob/master/menue.html",
+            url: "https://javabotchi.kunst-werk-hagen.de/menue.html",
             position: {
                 vertical: "top",
                 horizontal: "middle",
