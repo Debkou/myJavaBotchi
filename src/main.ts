dcb63869bff7537078fc0489e5ef79217615628d
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-24 10:38:27 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,44 +2,24 @@
 
 console.log('Script started successfully');
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
-
-let currentPopup: any = undefined;
-let noteWebsite: any;
-
-// Funktion zum Schließen des Popups
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ', WA.player.tags);
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    });
-
-    WA.room.area.onLeave('clock').subscribe(closePopup);
+    let noteWebsite: any;
 
     WA.room.onEnterLayer("visibleNote").subscribe(async () => {
         console.log("Entering visibleNote layer");
 
         noteWebsite = await WA.ui.website.open({
-            url: "https://javabotchi.kunst-werk-hagen.de/menue.html",
+            url: "./note.html",
             position: {
                 vertical: "top",
                 horizontal: "middle",
             },
             size: {
-                height: "60vh",
-                width: "20vw",
+                height: "30vh",
+                width: "50vw",
             },
             margin: {
                 top: "10vh",
@@ -50,16 +30,8 @@ WA.onInit().then(() => {
     });
 
     WA.room.onLeaveLayer("visibleNote").subscribe(() => {
-               console.log("leave visibleNote layer");
-                noteWebsite.close();
-       
+        noteWebsite.close();
     });
- 
-
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
 
