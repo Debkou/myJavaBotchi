1d0c6045757e14a51c36dbca9cb61afa3c883e5d
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-07-26 12:57:13 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,32 +5,19 @@ console.log('Script started successfully');
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 let currentPopup: any = undefined;
+let noteWebsite: any = undefined;
 
-// Funktion zum Schließen des Popups
-function closePopup(){
+// Function to close the popup
+function closePopup() {
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
     }
 }
 
-// Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ', WA.player.tags);
-
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    });
-
-    WA.room.area.onLeave('clock').subscribe(closePopup);
- 
- let noteWebsite: any = undefined;
-    WA.room.onEnterLayer("terminalAktion").subscribe(async () => {
-        console.log("Entering visibleNote layer");
-
+// Function to open the note website if not already opened
+async function openNoteWebsite() {
+    if (!noteWebsite) {
         noteWebsite = await WA.ui.website.open({
             url: "./menue.html",
             visible: true,
@@ -47,7 +34,27 @@ WA.onInit().then(() => {
             },
             allowApi: true,
         });
+    } else {
+        noteWebsite.open(); // Open if already created
+    }
+}
 
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ', WA.player.tags);
+
+    WA.room.area.onEnter('clock').subscribe(() => {
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    });
+
+    WA.room.area.onLeave('clock').subscribe(closePopup);
+
+    WA.room.onEnterLayer("terminalAktion").subscribe(() => {
+        console.log("Entering visibleNote layer");
+        openNoteWebsite();
     });
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
