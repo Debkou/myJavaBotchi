6496016663f7179147733a56d7682f89ef7052bb
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-11 12:38:23 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -183,7 +183,7 @@ WA.onInit().then(() => {
 
                 // Ebene aktionLevel1 einblenden
                 WA.room.setProperty("exitLevel1", "exitSceneUrl", "office.tmj");
-                WA.ui.modal.closeModal("Passwort Eingabe");
+                WA.ui.modal.closeModal();
             
             } else {
                 ergebnisElement.textContent = data.result;
