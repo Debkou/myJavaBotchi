ea19375d56026d1f5c57960663b977f63485053a
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-11 12:30:30 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -183,6 +183,7 @@ WA.onInit().then(() => {
 
                 // Ebene aktionLevel1 einblenden
                 WA.room.setProperty("exitLevel1", "exitSceneUrl", "office.tmj");
+                WA.ui.modal.closeModal(): void
             
             } else {
                 ergebnisElement.textContent = data.result;
