810c2734136de299bd3d13d1225650726c72a210
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-15 15:59:58 +0200

    Update levelEinsMain.ts

--- src/levelEinsMain.ts
+++ src/levelEinsMain.ts
@@ -4,68 +4,19 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
-// Funktion zum Schließen des Popups
-function closePopup() {
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 // Warten, bis die API bereit ist
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ', WA.player.tags);
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    });
-
-    WA.room.area.onLeave('clock').subscribe(closePopup);
-
-    WA.room.onEnterLayer("terminalAktion").subscribe(async () => {
-        console.log("Entering terminalAktion layer");
-        WA.ui.modal.openModal({
-            title: "Hauptmenue",
-            src: './menue.html',
-            allow: "fullscreen",
-            allowApi: true,
-            position: "center",
-        });
-    });
-
-    WA.room.onEnterLayer("buecherAktion").subscribe(async () => {
-        console.log("Entering buecherAktion layer");
-        WA.ui.modal.openModal({
-            title: "Bibliothek",
-            src: './bibliothek.html',
-            allow: "fullscreen",
-            allowApi: true,
-            position: "center",
-        });
-    });
-
-    WA.room.onEnterLayer("aktionFlyer").subscribe(async () => {
-        console.log("Entering aktionFlyer layer");
-        WA.room.showLayer('magentaFlyer');
-    });
-
-    WA.room.onLeaveLayer("aktionFlyer").subscribe(async () => {
-        console.log("Leaving aktionFlyer layer");
-        WA.room.hideLayer('magentaFlyer');
-    });
-
-    WA.room.area.onEnter("feldTasteFlyer").subscribe(() => {
+   WA.room.area.onEnter("areaAktionTerminal").subscribe(() => {
         const triggerMessage = WA.ui.displayActionMessage({
-            message: "Drücke 'SPACE' um den Flyer zu sehen",
+            message: "Drücke 'SPACE' um das Hauptmenü zu öffnen",
             callback: () => {
                 WA.ui.modal.openModal({
-                    title: "Flyer",
-                    src: './flyer_party.html',
+                    title: "Terminal",
+                    src: './menue.html',
                     allow: "fullscreen",
                     allowApi: true,
                     position: "center",
@@ -73,83 +24,7 @@ WA.onInit().then(() => {
             }
         });
 
-        WA.room.area.onLeave("feldTasteFlyer").subscribe(() => {
-            triggerMessage.remove();
-        });
-    });
-
-    WA.room.area.onEnter("feldTasteApi").subscribe(() => {
-        const triggerMessage = WA.ui.displayActionMessage({
-            message: "Drücke 'SPACE' um die Oracle Java API zu öffnen",
-            callback: () => {
-                WA.ui.modal.openModal({
-                    title: "Oracle Java API",
-                    src: 'https://docs.oracle.com/en/java/javase/11/docs/api/',
-                    allow: "fullscreen",
-                    allowApi: true,
-                    position: "center",
-                });
-            }
-        });
-
-        WA.room.area.onLeave("feldTasteApi").subscribe(() => {
-            triggerMessage.remove();
-        });
-    });
-
-      WA.room.area.onEnter("feldTasteHin").subscribe(() => {
-        const triggerMessage = WA.ui.displayActionMessage({
-            message: "Drücke 'SPACE' um die Hinweise zu öffnen",
-            callback: () => {
-                WA.ui.modal.openModal({
-                    title: "Hinweis anzeigen",
-                    src: './hinweis1.html', // Dein HTML-Dokument
-                    allow: "fullscreen",
-                    allowApi: true,
-                    position: "center",
-                });
-            }
-        });
-
-        WA.room.area.onLeave("feldTasteHin").subscribe(() => {
-            triggerMessage.remove();
-        });
-    });
-    
-    WA.room.area.onEnter("feldTastePong").subscribe(() => {
-        const triggerMessage = WA.ui.displayActionMessage({
-            message: "Drücke 'SPACE' um PingPong zu spielen",
-            callback: () => {
-                WA.ui.modal.openModal({
-                    title: "PingPong",
-                    src: 'https://www.spiele123.com/spiel/ping-pong',
-                    allow: "fullscreen",
-                    allowApi: true,
-                    position: "center",
-                });
-            }
-        });
-
-        WA.room.area.onLeave("feldTastePong").subscribe(() => {
-            triggerMessage.remove();
-        });
-    });
-
-    WA.room.area.onEnter("feldAktionEingabe").subscribe(() => {
-        const triggerMessage = WA.ui.displayActionMessage({
-            message: "Drücke 'SPACE' um die Tür zu öffnen",
-            callback: () => {
-                WA.ui.modal.openModal({
-                    title: "Passwort Eingabe",
-                    src: './eingabeTest.html', // Dein HTML-Dokument
-                    allow: "fullscreen",
-                    allowApi: true,
-                    position: "center",
-                });
-            }
-        });
-
-        WA.room.area.onLeave("feldAktionEingabe").subscribe(() => {
+        WA.room.area.onLeave("areaAktionTerminal").subscribe(() => {
             triggerMessage.remove();
         });
     });
