4019ee72185391a66e65cd7e63976004dded9962
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-15 22:31:11 +0200

    Update levelEinsMain.ts

--- src/levelEinsMain.ts
+++ src/levelEinsMain.ts
@@ -97,6 +97,44 @@ aktionsFeld("areaAktionGitter", "Drücke 'SPACE' um die Tür zu öffnen", "Gitte
     // Event-Listener für den "Licht an" Button
     const lichtButton = document.getElementById("lichtButton") as HTMLButtonElement;
     lichtButton.addEventListener("click", ueberpruefePasswort);
+
+    // Funktion zur Überprüfung des Zahlenschlosses
+async function ueberpruefeZahlenschloss() {
+    const eingabeElement = document.getElementById("eingabeGitter") as HTMLInputElement;
+    const eingabe = eingabeElement.value.trim();
+    const ergebnisElement = document.getElementById("ergebnis") as HTMLElement;
+
+    try {
+        const response = await fetch(`https://javabotchi.kunst-werk-hagen.de/apiTest.php?name=zahlenschloss`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ eingabe })
+        });
+
+        if (!response.ok) {
+            throw new Error('Netzwerkantwort war nicht ok.');
+        }
+
+        const data = await response.json();
+
+        if (data.result === 'Korrekt!') {
+            ergebnisElement.innerHTML = `<p style="color: green;">${data.result}</p>`;
+            // Weitere Logik, z.B. das Öffnen der Tür, könnte hier hinzugefügt werden
+            // Schließe eventuell das Modal oder zeige eine Erfolgsmeldung an
+        } else {
+            ergebnisElement.innerHTML = `<p style="color: red;">${data.result}</p>`;
+        }
+    } catch (error) {
+        ergebnisElement.innerHTML = `<p><span style="font-family: pokemon;" class="dBlau-font">Fehler:</span> <br> Fehler beim Überprüfen des Zahlenschlosses. Bitte versuche es später erneut.</p>`;
+        console.error('Es gab ein Problem mit der Anfrage:', error);
+    }
+}
+
+// Event-Listener für den "Gitter Tür" Button
+const gitterButton = document.getElementById("gitterTuer") as HTMLButtonElement;
+gitterButton.addEventListener("click", ueberpruefeZahlenschloss);
    
 
     // Initialisierung der Scripting API Extra-Bibliothek
