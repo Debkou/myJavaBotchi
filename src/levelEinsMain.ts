363f8ed557752b2fedc8c4ff85048945b20fd7e4
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-19 23:51:02 +0200

    Update levelEinsMain.ts

--- src/levelEinsMain.ts
+++ src/levelEinsMain.ts
@@ -69,39 +69,6 @@ async function ueberpruefePasswort() {
     }
 }
 
-// Funktion zur Überprüfung des Zahlenschlosses
-async function ueberpruefeZahlenschloss() {
-    const eingabeElement = document.getElementById("eingabeGitter") as HTMLInputElement;
-    const eingabe = eingabeElement.value.trim();
-    const ergebnisElement = document.getElementById("ergebnis") as HTMLElement;
-
-    try {
-        const response = await fetch('https://javabotchi.kunst-werk-hagen.de/apiTest.php?name=zahlenschloss', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ eingabe })
-        });
-
-        if (!response.ok) {
-            throw new Error('Netzwerkantwort war nicht ok.');
-        }
-
-        const data = await response.json();
-
-        if (data.result === 'Korrekt!') {
-            ergebnisElement.innerHTML = `<p style="color: green;">${data.result}</p>`;
-            // Weitere Logik, z.B. das Öffnen der Tür, könnte hier hinzugefügt werden
-        } else {
-            ergebnisElement.innerHTML = `<p style="color: red;">${data.result}</p>`;
-        }
-    } catch (error) {
-        ergebnisElement.innerHTML = `<p><span style="font-family: pokemon;" class="dBlau-font">Fehler:</span> <br> Fehler beim Überprüfen des Zahlenschlosses. Bitte versuche es später erneut.</p>`;
-        console.error('Es gab ein Problem mit der Anfrage:', error);
-    }
-}
-
 // Warten, bis die API bereit ist
 WA.onInit().then(() => {
     console.log('Scripting API ready');
@@ -125,6 +92,7 @@ WA.onInit().then(() => {
     aktionsFeld("p4a", "Drücke 'SPACE' um den Hinweis zu öffnen", "Terminal", './postIt4a.html');
     aktionsFeld("p4b", "Drücke 'SPACE' um den Hinweis zu öffnen", "Terminal", './postIt4b.html');
     aktionsFeld("areaAktionGitter", "Drücke 'SPACE' um die Tür zu öffnen", "Gittertür", './levelEinsGitter.html');
+    aktionsFeld("aktionSchlueSchrank", "Drücke 'SPACE' um den Schlüsselschrank zu öffnen", "Tresor", './levelEinsTresor.html');
 
     // Event-Listener für den "Licht an" Button
     const lichtButton = document.getElementById("lichtButton") as HTMLButtonElement;
