1ee91c65e450ae11a00d304d10b7ca0185e525aa
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-23 00:28:55 +0200

    Update levelZweiMain.ts

--- src/levelZweiMain.ts
+++ src/levelZweiMain.ts
@@ -5,7 +5,7 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 // Boolesche Variable
-let isTestPassed: boolean = true;
+let isTestPassed: boolean = false;
 
 // Funktion zur Registrierung des Aktionsbereichs
 function aktionArea(
