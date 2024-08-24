6b721e314391bddd74aaf01f6f05afce046aaccb
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-25 00:19:32 +0200

    Update levelDreiMain.ts

--- src/levelDreiMain.ts
+++ src/levelDreiMain.ts
@@ -31,6 +31,42 @@ function aktionArea(
     });
 }
 
+// Funktion zur Überprüfung des Zahlenschlosses
+async function phoneCode() {
+    const eingabeElement = document.getElementById("code") as HTMLInputElement;
+    const eingabe = eingabeElement.value.trim();
+    const ergebnisElement = document.getElementById("ergebnis") as HTMLElement;
+
+    try {
+        const response = await fetch(`https://javabotchi.kunst-werk-hagen.de/apiTest.php?name=PhoneCode`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ eingabe })
+        });
+
+        if (!response.ok) {
+            throw new Error('Netzwerkantwort war nicht ok.');
+        }
+
+        const data = await response.json();
+
+        if (data.result === 'Korrekt!') {
+            ergebnisElement.innerHTML = `<p style="color: green;">${data.result}</p>`;
+            // Ändere den Status der Gittertür
+          aktionArea("areaTelefon", "Drücke 'SPACE' um das Telefon zu nutzen", "Telefon", './levelDreiBrute.html');
+                  setTimeout(() => {
+                WA.ui.modal.closeModal();
+            }, 3000); // 3000 Millisekunden = 3 Sekunden
+        } else {
+            ergebnisElement.innerHTML = `<p style="color: red;">${data.result}</p>`;
+        }
+    } catch (error) {
+        ergebnisElement.innerHTML = `<p><span style="font-family: pokemon;" class="dBlau-font">Fehler:</span> <br> Fehler beim Überprüfen des Zahlenschlosses. Bitte versuche es später erneut.</p>`;
+        console.error('Es gab ein Problem mit der Anfrage:', error);
+    }
+}
 
 
 // Warten, bis die API bereit ist
@@ -49,6 +85,8 @@ WA.onInit().then(() => {
       WA.room.hideLayer("magentaKabel");
     });
 
+      const codeBtn = document.getElementById("codeBtn") as HTMLButtonElement;
+    codeBtn.addEventListener("click", phoneCode);
 
     // Initialisierung der Scripting API Extra-Bibliothek
     bootstrapExtra().then(() => {
