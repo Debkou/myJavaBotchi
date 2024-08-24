d2bcd24b6e9e6b861d96a86cd9ed2cbd43d234a7
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-24 20:51:18 +0200

    Update levelDreiMain.ts

--- src/levelDreiMain.ts
+++ src/levelDreiMain.ts
@@ -36,48 +36,9 @@ WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ', WA.player.tags);
 
- document.getElementById("checkButton")?.addEventListener("click", () => {
-    // Auswahl der Radio-Buttons überprüfen
-    const question1Correct = (document.getElementById("option1-2") as HTMLInputElement).checked;
-    const question2Correct = (document.getElementById("option2-2") as HTMLInputElement).checked;
 
-    // Überprüfungsergebnis-Element
-    const ergebnisElement = document.getElementById("ergebnis");
-
-    if (question1Correct && question2Correct) {
-        // Wenn beide Antworten korrekt sind
-        ergebnisElement!.innerHTML = "<p>Korrekt!<br>Passwort: Reparieren</p>";
-        
-    } else {
-        // Anzahl der richtigen und falschen Antworten berechnen
-        let correctAnswers = 0;
-        let incorrectAnswers = 0;
-
-        if (question1Correct) {
-            correctAnswers++;
-        } else {
-            incorrectAnswers++;
-        }
-
-        if (question2Correct) {
-            correctAnswers++;
-        } else {
-            incorrectAnswers++;
-        }
-
-        // Anzeige der Anzahl der richtigen und falschen Antworten
-        ergebnisElement!.innerHTML = `<p>Du hast ${correctAnswers} richtige und ${incorrectAnswers} falsche Antworten.</p><p>Bitte versuche es erneut.</p>`;
-    }
-});
     aktionArea("areaAnleitung", "Drücke 'SPACE' um die Anleitung zu lesen", "Anleitung", './levelZweiAnleitung.html');
-    aktionArea("areaAnleitungCode", "Drücke 'SPACE' um den Code anzuzeigen", "AnleitungCode", './levelZweiHeizungCode.html');
-    aktionArea("areaUhr", "Drücke 'SPACE' um den Code anzuzeigen", "AnleitungCode", './levelZweiGUIFail.html');
-    aktionArea("areaTerminalEins", "Drücke 'SPACE' um das Hauptmenü zu öffnen", "Terminal", './menue.html'); 
-    aktionArea("areaRad", "Drücke 'SPACE' um das Rad zu suchen", "Schrank", './levelZweiRad.html');
-    aktionArea("areaInfoWagen", "Drücke 'SPACE' um den Wagen zu schieben.", "Wagen", './levelZweiWagen.html');
-    aktionArea("areaInfoHandy", "Drücke 'SPACE' um die Info anzuzeigen.", "Fundbüro", './levelZweiFundbuero.html');
-    aktionArea("areaAktionHandy", "Drücke 'SPACE' um den Schrank zu Öffnen", "Schrank", './levelZweiPhone.html');
-    aktionArea("areaZentraleTuer", "Drücke 'SPACE' um die Tür zu Öffnen", "Haustechnik", './levelZweiFinalEingabe.html');
+
 
 
 
