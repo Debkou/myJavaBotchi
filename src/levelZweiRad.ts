f4532d9fa04d6123f1523f67f70dd17951d7f965
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-26 01:19:26 +0200

    Update levelZweiRad.ts

--- src/levelZweiRad.ts
+++ src/levelZweiRad.ts
@@ -4,17 +4,18 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-
 // Warten, bis die API bereit ist
 WA.onInit().then(() => {
     console.log('Scripting API ready');
 
    document.addEventListener('DOMContentLoaded', () => {
-    // Den Button-Element selektieren
+    
     const button = document.getElementById('btRad');
+    const ergebnisElement = document.getElementById("ergebnis") as HTMLElement;
 
-    // Event-Listener für den Klick auf den Button hinzufügen
+    // Event-Listener für den Button 
     button?.addEventListener('click', () => {
+    ergebnisElement.innerHTML = `<p">Du hast das Rad eingesteckt!</p>`;
         // Die Funktion zum Ausblenden der Ebene aufrufen
         WA.room.hideLayer('wagenVor');
         WA.room.showLayer('wagenWeg');
@@ -22,7 +23,6 @@ WA.onInit().then(() => {
         WA.room.area.delete('areaInfoWagen');
     });
 });
-    // Initialisierung der Scripting API Extra-Bibliothek
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
