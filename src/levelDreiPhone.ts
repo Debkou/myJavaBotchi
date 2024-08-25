6e9507efcbcca94cabeaeefe68f2f644e2c29df0
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-25 17:35:03 +0200

    Update levelDreiPhone.ts

--- src/levelDreiPhone.ts
+++ src/levelDreiPhone.ts
@@ -4,65 +4,54 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
+// Funktion zur Überprüfung des Zahlenschlosses
+async function phoneCode() {
+    const eingabeElement = document.getElementById("code") as HTMLInputElement;
+    const eingabe = eingabeElement.value.trim();
+    const ergebnisElement = document.getElementById("ergebnis") as HTMLElement;
 
+    try {
+        const response = await fetch(`https://javabotchi.kunst-werk-hagen.de/apiTest.php?name=PhoneCode`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ eingabe })
+        });
+
+        if (!response.ok) {
+            throw new Error('Netzwerkantwort war nicht ok.');
+        }
+
+        const data = await response.json();
+
+        if (data.result === 'Korrekt!') {
+            ergebnisElement.innerHTML = `<p style="color: green;">${data.result}</p>`;
+             ergebnisElement.innerHTML = `<p>"Na Super! Kein Empfang. Ich brauche ein Telefon!"</p>`;
+             WA.room.showLayer("aktionTelefon"); 
+            WA.room.setProperty("aktionTelefon", "openWebsite", "./levelDreiTelefon.html");
+   
+            // Schließe das Modal nach 3 Sekunden und öffne dann die neue Seite
+            setTimeout(() => {
+                WA.ui.modal.closeModal();
+            }, 4000); // 4000 Millisekunden = 4 Sekunden
+        } else {
+            ergebnisElement.innerHTML = `<p style="color: red;">${data.result}</p>`;
+        }
+    } catch (error) {
+        ergebnisElement.innerHTML = `<p><span style="font-family: pokemon;" class="dBlau-font">Fehler:</span> <br> Fehler beim Überprüfen des Zahlenschlosses. Bitte versuche es später erneut.</p>`;
+        console.error('Es gab ein Problem mit der Anfrage:', error);
+    }
 // Warten, bis die API bereit ist
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ', WA.player.tags);
 
 
-    // Hier kommt die Logik für die Passwortüberprüfung und das Einblenden der Ebene
-    const eingabeElement = document.getElementById("eingabe") as HTMLInputElement;
-    const submitButton = document.getElementById("submitButton") as HTMLButtonElement;
-    const ergebnisElement = document.getElementById("ergebnis") as HTMLElement;
 
-    // Funktion zur Überprüfung des Passworts
-    async function ueberpruefePasswort() {
-        const eingabe = eingabeElement.value.trim();
 
-        try {
-            const response = await fetch(`https://javabotchi.kunst-werk-hagen.de/apiTest.php?name=LevelEinsTuer`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ eingabe })
-            });
-
-            if (!response.ok) {
-                throw new Error('Netzwerkantwort war nicht ok.');
-            }
-
-            const data = await response.json();
-
-            if (data.result === 'Korrekt!') {
-                ergebnisElement.textContent = data.result;
-                ergebnisElement.className = 'correct';
-
-                // Ebene aktionLevel1 einblenden
-                WA.room.setProperty("exitLevelEins", "exitSceneUrl", "flur.tmj");
-                WA.ui.modal.closeModal();
-            
-            } else {
-                ergebnisElement.textContent = data.result;
-                ergebnisElement.className = 'incorrect';
-            }
-        } catch (error) {
-            ergebnisElement.textContent = 'Fehler beim Überprüfen des Passworts. Bitte versuche es später erneut.';
-            ergebnisElement.className = 'error';
-            console.error('Es gab ein Problem mit der Anfrage:', error);
-        }
-    }
-
-    // Event-Listener für den Button
-    submitButton.addEventListener("click", ueberpruefePasswort);
-
-    // Event-Listener für die Enter-Taste
-    document.addEventListener("keydown", function(event) {
-        if (event.key === "Enter") {
-            ueberpruefePasswort();
-        }
-    });
+  const telBtn = document.getElementById("nummerBtn") as HTMLButtonElement;
+    telBtn.addEventListener("click", telefon);
 
    
 
