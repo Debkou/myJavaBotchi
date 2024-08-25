a6ad5d95e5e6396c846917b0efedecb01871e2b1
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-26 00:14:07 +0200

    Add files via upload

---
+++ src/levelVierHenning.ts
@@ -0,0 +1,30 @@
+/// <reference types="@workadventure/iframe-api-typings" />
+
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+console.log('Script started successfully');
+
+
+// Warten, bis die API bereit ist
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+
+   document.addEventListener('DOMContentLoaded', () => {
+    // Den Button-Element selektieren
+    const button = document.getElementById('btKoala');
+
+    // Event-Listener für den Klick auf den Button hinzufügen
+    button?.addEventListener('click', () => {
+        // Die Funktion zum Ausblenden der Ebene aufrufen
+        WA.room.hideLayer('kassenbandBlock');
+        WA.room.showLayer('aktionKassenband');
+    });
+});
+    // Initialisierung der Scripting API Extra-Bibliothek
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+}).catch(e => console.error(e));
+
+export {};
