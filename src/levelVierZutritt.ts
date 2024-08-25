c47fc48f943278d3b7f89252a2f4044cd402ba8d
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-25 22:12:33 +0200

    Update levelVierZutritt.ts

--- src/levelVierZutritt.ts
+++ src/levelVierZutritt.ts
@@ -28,6 +28,9 @@ async function ueberpruefeZahlenschloss() {
 
         if (data.result === 'Korrekt!') {
             ergebnisElement.innerHTML = `<p style="color: green;">${data.result}</p>`;
+             setTimeout(() => {
+                WA.room.setProperty("aktionArchiv", "exitSceneUrl", "archiv.tmj");
+            }, 3000); // 3000 Millisekunden = 3 Sekunden
           
         } else {
             ergebnisElement.innerHTML = `<p style="color: red;">${data.result}</p>`;
