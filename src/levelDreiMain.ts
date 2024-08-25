2dbd68c30010dc0837c26b1837648addcc32fc32
Dehbia Kouadria <105669754+Debkou@users.noreply.github.com>
2024-08-25 17:46:25 +0200

    Update levelDreiMain.ts

--- src/levelDreiMain.ts
+++ src/levelDreiMain.ts
@@ -72,43 +72,6 @@ async function phoneCode() {
     }
 }
 
-// Funktion zur Überprüfung des Zahlenschlosses
-async function telefon() {
-    const eingabeElement = document.getElementById("nummer") as HTMLInputElement;
-    const eingabe = eingabeElement.value.trim();
-    const ergebnisElement = document.getElementById("ergebnis") as HTMLElement;
-
-    try {
-        const response = await fetch(`https://javabotchi.kunst-werk-hagen.de/apiTest.php?name=Telefonnummer`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ eingabe })
-        });
-
-        if (!response.ok) {
-            throw new Error('Netzwerkantwort war nicht ok.');
-        }
-
-        const data = await response.json();
-
-        if (data.result === 'Korrekt!') {
-            ergebnisElement.innerHTML = `<p style="color: green;">${data.result}</p>`;
-  
-            // Schließe das Modal nach 4 Sekunden und öffne dann die neue Seite
-            setTimeout(() => {
-                WA.ui.modal.closeModal();
-            }, 4000); // 4000 Millisekunden = 4 Sekunden
-        } else {
-            ergebnisElement.innerHTML = `<p style="color: red;">${data.result}</p>`;
-        }
-    } catch (error) {
-        ergebnisElement.innerHTML = `<p><span style="font-family: pokemon;" class="dBlau-font">Fehler:</span> <br> Fehler beim Überprüfen des Zahlenschlosses. Bitte versuche es später erneut.</p>`;
-        console.error('Es gab ein Problem mit der Anfrage:', error);
-    }
-}
-
 // Warten, bis die API bereit ist
 WA.onInit().then(() => {
     console.log('Scripting API ready');
@@ -166,9 +129,6 @@ WA.onInit().then(() => {
 
       const codeBtn = document.getElementById("codeBtn") as HTMLButtonElement;
     codeBtn.addEventListener("click", phoneCode);
-
-    const telBtn = document.getElementById("nummerBtn") as HTMLButtonElement;
-    telBtn.addEventListener("click", telefon);
 
     // Initialisierung der Scripting API Extra-Bibliothek
     bootstrapExtra().then(() => {
